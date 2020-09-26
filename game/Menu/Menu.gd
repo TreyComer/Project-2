08ff3a30cf9829b16777a336f7f5752f731a4459
TreyComer <jaacomer@iu.edu>
2020-09-25 22:47:00 -0700

    c

--- game/Menu/Menu.gd
+++ game/Menu/Menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_Play_pressed():
-   get_tree().change_scene("res://Game.tscn")
+   get_tree().change_scene("res://game/Game.tscn")
 
 func _on_Quit_pressed():
   get_tree().quit()
--- game/Player/Player.gd
+++ game/Player/Player.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 export var speed = 4
 export var rotation_speed = 0.1
 
+onready var HUD = get_node("/root/Game/HUD")
 onready var Bullet = load("res://Bullet/Bullet.tscn")
 
 func _ready():
@@ -16,7 +17,7 @@ func _physics_process(delta):
 	rotation += mouse.angle()*rotation_speed
 	position += get_input()*speed
 	
-	if Input.is_action_pressed("shoot"):
+	if Input.is_action_just_pressed("shoot"):
 		var bullet =  Bullet.instance()
 		bullet.position = position
 		get_node("/root/Game/Bullets").add_child(bullet)
@@ -32,3 +33,11 @@ func get_input():
 	if Input.is_action_pressed("right"):
 		input_dir.x += 1
 	return input_dir.rotated(rotation)
+
+
+func _on_Level2_body_entered(body):
+	pass # Replace with function body.
+	
+func _on_Damage_body_entered(body):
+   HUD.update_health(-body.damage)
+   body.die()
